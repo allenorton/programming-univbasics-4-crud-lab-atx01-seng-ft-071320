93a8c2bcba936745cda868a8a2b403c4ebcef576
Allene Norton <norton.allene@gmail.com>
2020-06-10 19:29:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  pets = [smoosh, olly, sammie, derp]
+  pets = ['smoosh', 'olly', 'sammie', 'derp']
 end
 
 def add_element_to_end_of_array(array, element)
